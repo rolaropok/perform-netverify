0c8ca64dac53659c1eaf4e7495cd15cd2b0ebe9c
rolaropok <rolaropok@gmail.com>
2020-08-05 08:51:10 -0400

    change to typescript

--- perform-netverify.ts
+++ perform-netverify.ts
@@ -1,49 +1,57 @@
-const https = require('https')
+import https from 'https';
 
-const API_TOKEN = process.env.API_TOKEN
-const API_SECRET = process.env.API_SECRET
+const API_TOKEN: string = process.env.API_TOKEN;
+const API_SECRET: string = process.env.API_SECRET;
 
-function validateRequest(data) {
-  if (!data) {
-    throw new Error('request body is empty')
-  }
-  if (!data.merchantIdScanReference) {
-    throw new Error('merchantIdScanReference is required')
-  }
-  if (!data.frontsideImage) {
-    throw new Error('frontsideImage is required')
-  }
-  if (!data.faceImage) {
-    throw new Error('faceImage is required')
-  }
-  if (!data.country) {
-    throw new Error('country is required')
-  }
-  if (!data.idType) {
-    throw new Error('idType is required')
-  }
-
-  return null
+interface ReqData {
+  merchantIdScanReference: string;
+  frontsideImage: string;
+  faceImage: string;
+  country: string;
+  idType: string;
 }
 
-module.exports = function(data) {
+function validateRequest(data: ReqData) {
+  if (!data) {
+    throw new Error('request body is empty');
+  }
+  if (!data.merchantIdScanReference) {
+    throw new Error('merchantIdScanReference is required');
+  }
+  if (!data.frontsideImage) {
+    throw new Error('frontsideImage is required');
+  }
+  if (!data.faceImage) {
+    throw new Error('faceImage is required');
+  }
+  if (!data.country) {
+    throw new Error('country is required');
+  }
+  if (!data.idType) {
+    throw new Error('idType is required');
+  }
+
+  return null;
+}
+
+export default function(data: ReqData) {
   return new Promise((resolve, reject) => {
     try {
-      validateRequest(data)
+      validateRequest(data);
     } catch (error) {
       resolve({
         status: 'error',
         code: 400,
         message: error.message
-      })
-      return
+      });
+      return;
     }
 
-    const merchantIdScanReference = data.merchantIdScanReference
-    const frontsideImage = data.frontsideImage
-    const faceImage = data.faceImage
-    const country = data.country
-    const idType = data.idType
+    const merchantIdScanReference = data.merchantIdScanReference;
+    const frontsideImage = data.frontsideImage;
+    const faceImage = data.faceImage;
+    const country = data.country;
+    const idType = data.idType;
 
     const body = {
       merchantIdScanReference,
@@ -51,11 +59,11 @@ module.exports = function(data) {
       faceImage,
       country,
       idType
-    }
+    };
 
-    const bodyStr = JSON.stringify(body)
+    const bodyStr = JSON.stringify(body);
 
-    const authorization = Buffer.from(`${API_TOKEN}:${API_SECRET}`).toString('base64')
+    const authorization = Buffer.from(`${API_TOKEN}:${API_SECRET}`).toString('base64');
     const options = {
       hostname: 'netverify.com',
       path: '/api/netverify/v2/performNetverify',
@@ -67,23 +75,23 @@ module.exports = function(data) {
         'Content-Length': Buffer.byteLength(bodyStr),
         'Authorization': `Basic ${authorization}`
       }
-    }
+    };
     https.request(options, res => {
-      let data = ''
-      res.on('data', d => data += d)
+      let data = '';
+      res.on('data', d => data += d);
       res.on('end', () => {
-        const response = JSON.parse(data)
+        const response = JSON.parse(data);
         if (response.httpStatus !== 200) {
           resolve({
             status: 'error',
             code: response.httpStatus,
             message: response.message
-          })
+          });
         } else {
           resolve({
             status: 'success',
             data: response.data
-          })
+          });
         }
       })
     }).on('error', error => {
@@ -91,7 +99,7 @@ module.exports = function(data) {
         status: 'error',
         code: 400,
         message: error.message
-      })
-    }).end(bodyStr)
-  })
+      });
+    }).end(bodyStr);
+  });
 }
